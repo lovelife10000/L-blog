0ac5107d69ffc9bae2ac81977732aebda55b128b
lovelife10000 <1358180015@qq.com>
2017-04-20 10:17:11 +0800

    更新项目名

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# blog
+# L-blog
 基于nodejs、mongodb、express、angularjs、bootstrap等搭建的blog内容管理系统
--- routers/admin.js
+++ routers/admin.js
@@ -4,7 +4,7 @@
 var express=require('express');
 var router=express.Router();
 
-router.get('/user',function (req,res,next) {
+router.get('/',function (req,res,next) {
   res.send('User');
 });
 
