be9d0db73b1c0b01bccdb856f0b49828f5d936b3
lovelife10000 <1358180015@qq.com>
2017-04-20 19:54:05 +0800

    后台功能划分、页面搭建

--- routers/admin.js
+++ routers/admin.js
@@ -5,7 +5,40 @@ var express=require('express');
 var router=express.Router();
 
 router.get('/',function (req,res,next) {
-  res.send('User');
+  res.render('admin/admin');
+});
+//所有用户组
+router.get('/users_group',function (req,res,next) {
+  res.render('admin/users_group');
 });
 
+//添加用户组
+router.get('/users_group_add',function (req,res,next) {
+  res.render('admin/users_group_add');
+});
+
+//所有用户
+router.get('/users',function (req,res,next) {
+  res.render('admin/users');
+});
+
+//添加用户
+router.get('/users_add',function (req,res,next) {
+  res.render('admin/users_add');
+});
+
+//登录记录
+router.get('/login_log',function (req,res,next) {
+  res.render('admin/login_log');
+});
+
+//所有文章
+router.get('/articles',function (req,res,next) {
+  res.render('admin/articles');
+});
+
+//文章分类
+router.get('/articles_categories',function (req,res,next) {
+  res.render('admin/articles_categories');
+});
 module.exports=router;
