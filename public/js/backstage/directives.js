ba4b2ba05d99e34a9ac34cfffdd84249f78f9f9d
lovelife10000 <1358180015@qq.com>
2017-04-26 09:21:31 +0800

    angularjs文件上传功能待实现

--- public/js/backstage/directives.js
+++ public/js/backstage/directives.js
@@ -20,3 +20,22 @@ app.directive('pwCheck', [function () {
     }
   }
 }]);
+
+//angular文件上传功能
+app.directive('fileModel', ['$parse', function ($parse) {
+    return {
+      restrict: 'A',
+      link: function(scope, element, attrs, ngModel) {
+        var model = $parse(attrs.fileModel);
+        var modelSetter = model.assign;
+        element.bind('change', function(event){
+          scope.$apply(function(){
+            modelSetter(scope, element[0].files[0]);
+          });
+          //附件预览
+          scope.file = (event.srcElement || event.target).files[0];
+          scope.getFile();
+        });
+      }
+    };
+  }]);
