10a16a42f2c5bac4e6461babb2be74236e90e2d6
lovelife10000 <1358180015@qq.com>
2017-05-09 18:05:27 +0800

    引入ueditor编辑器

--- app.js
+++ app.js
@@ -11,10 +11,22 @@ var cookieParser = require('cookie-parser');
 var session = require('express-session');
 var settings = require('./util/settings');
 var redisStore = require('connect-redis')(session);
+var path = require('path');
+var ueditor = require("ueditor");
 /*
  * 创建app应用
  * */
 var app = express();
+/*
+ * 设置bodyParser
+ * */
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(bodyParser.json());
+
+
+
 
 /*
  * 设置cookie
@@ -37,6 +49,35 @@ app.use(session({
   cookie: {maxAge: 30 * 60 * 1000}
 }));
 
+
+
+/*
+ * 设置静态文件托管
+ * */
+app.use('/public', express.static(__dirname + '/public'));
+
+/*
+ * 设置模版引擎
+ * */
+// app.engine('html', swig.renderFile);
+
+/*
+ * 设置视图文件夹
+ * */
+app.set('views', './views');
+
+/*
+ * 设置视图引擎格式
+ * */
+// app.set('view engine','html');
+app.set('view engine', 'ejs');
+
+
+/*
+ * 配置body-parser
+ * */
+app.use(bodyParser.urlencoded({extended: true}));
+/**********************************模块路由部分由下面开始********************************************/
 /*
  * 所有请求访问入口
  * */
@@ -63,39 +104,45 @@ app.use('/', function (req, res, next) {
 
   next();
 });
-
-/*
- * 设置静态文件托管
- * */
-app.use('/public', express.static(__dirname + '/public'));
-
-/*
- * 设置模版引擎
- * */
-// app.engine('html', swig.renderFile);
-/*
- * 设置视图文件夹
- * */
-app.set('views', './views');
-/*
- * 设置视图引擎格式
- * */
-// app.set('view engine','html');
-app.set('view engine', 'ejs');
-
-
-/*
- * 配置body-parser
- * */
-app.use(bodyParser.urlencoded({extended: true}));
-
 /*
  * 创建模块
  * */
 app.use('/admin', require('./routers/admin'));
 app.use('/api', require('./routers/api'));
 app.use('/', require('./routers/index'));
+/*
+ * 配置ueditor路由
+ * */
 
+app.use("/public/plugins/ueditor/ue", ueditor(path.join(__dirname, 'public'), function (req, res, next) {
+
+  //客户端上传文件设置
+  var imgDir = '/img/ueditor/';
+  var ActionType = req.query.action;
+  if (ActionType === 'uploadimage' || ActionType === 'uploadfile' || ActionType === 'uploadvideo') {
+    var file_url = imgDir;//默认图片上传地址
+    /*其他上传格式的地址*/
+    if (ActionType === 'uploadfile') {
+      file_url = '/file/ueditor/'; //附件
+    }
+    if (ActionType === 'uploadvideo') {
+      file_url = '/video/ueditor/'; //视频
+    }
+    res.ue_up(file_url); //你只要输入要保存的地址 。保存操作交给ueditor来做
+    res.setHeader('Content-Type', 'text/html');
+  }
+  //  客户端发起图片列表请求
+  else if (req.query.action === 'listimage') {
+    var dir_url = imgDir;
+    res.ue_list(dir_url); // 客户端会列出 dir_url 目录下的所有图片
+  }
+  // 客户端发起其它请求
+  else {
+    // console.log('config.json')
+    res.setHeader('Content-Type', 'application/json');
+    res.redirect('/public/plugins/ueditor/nodejs/config.json');
+  }
+}));
 /*
  * 尝试测试数据库是否联通
  * */
--- models/Post.js
+++ models/Post.js
@@ -9,12 +9,15 @@ var PostSchema=Schema({
   post_publish_date:String,
   post_category:String,
   post_from:Number,
-  post_type:String
+  post_type:String,
   post_recommend:Boolean,
   post_view:Number,
   post_comment_num:Number,
   post_display:Boolean,
   post_author:String,
+  post_tags:String,
+  post_keywords:String,
+  post_discription:String,
 });
 var Post=mongoose.model('Posts',PostSchema);
 module.exports=Post;
