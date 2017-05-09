e98b3ea2fb77ba72194518beb028bd144659f33d
lovelife10000 <1358180015@qq.com>
2017-05-09 19:04:39 +0800

    获取ueditor中的html

--- public/js/controllers/L-blog.controller.js
+++ public/js/controllers/L-blog.controller.js
@@ -229,3 +229,17 @@ app.controller('categoriesAdd', ['$scope', '$http', function ($scope, $http) {
 
   };
 }]);
+
+/*
+ * 添加文章
+ * */
+app.controller('articlesAdd', ['$scope', '$http', function ($scope, $http) {
+  $scope.articleAdd = function () {
+    var html='';
+    ue.ready(function() {
+      html = ue.getContent();
+      console.log(html);
+    });
+
+  };
+}]);
